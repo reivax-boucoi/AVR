9c6e93d0f01a990f41342c0dbf904bb7d071e41a
reivax-boucoi <reivax.boucoi@laposte.net>
2017-10-21 23:44:59 +0200

    updated m32 projects

--- _m32/digital_filter/main.c
+++ _m32/digital_filter/main.c
@@ -13,12 +13,25 @@
     UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);	// 8bit data format
     UCSRB |= (1 << RXCIE ); // Enable the USART Recieve Complete interrupt
 }
-
-void uart_transmit (unsigned char data){
+volatile uint8_t test=0;
+void ADC_init(void){
+	ADMUX =0;//sets right adjusted, AREF referenced, ADC0 measurement channel
+	DDRA &=~(1<<PINA0);
+	ADCSRA |=(1<<ADEN)|(1<<ADIE)|(1<<ADATE); 
+	SFIOR|=(1<<ADTS2);	//set adc to run on timer0 ovf
+}
+void DAC_init(void){
+	DDRB |= 0b11111111;	
+}
+void uart_transmit(unsigned char data){
     while (!( UCSRA & (1<<UDRE)));	// wait while register is free
     UDR = data;	// load data in the register
 }
-
+void uart_transmitMult(char *Data){
+	while(*Data>0){
+		uart_transmit(*Data++);
+	}
+}
 unsigned char uart_recieve (void){
     while(!(UCSRA) & (1<<RXC));	// wait while data is being received
     return UDR;	// return 8-bit data
@@ -26,38 +39,36 @@ unsigned char uart_recieve (void){
 
 volatile uint8_t cnt=0;
 int main(void){
-  DDRB |= 1 << PINB0 ;
-  PORTB |= 1<<PINB0;
   uart_init();
+  DAC_init();
+  ADC_init();
   TIMSK|=1<<TOIE0;
   sei();
-  TCCR0 |=(1<<CS02) |(1<<CS00);
+  TCCR0 |=(1<<CS02)|(0<<CS01)|(0<<CS00);
   
   while(1){
-	/*uint8_t data = uart_recieve ();
-	if(data){
-		uart_transmit(data);
-		data=0;
-		}*/
+
   }
   return 0;
 }
 ISR(TIMER0_OVF_vect){
-	/*if(cnt>=46){
-		uart_transmit (0x41);
-		uart_transmit ('\n');
-		uart_transmit (0x42);
-		uart_transmit (0x0A);
-		PORTB ^= 1<<PINB0 ;
-		cnt=0;
-	}else{
-		cnt++;
-	}*/
+	if(test>0){
+		uart_transmitMult("help!");
+	}
+	test=1;			
 }
 
-ISR ( USART_RXC_vect )
-{
-char ReceivedByte ;
-ReceivedByte = UDR ;
-UDR = ReceivedByte ;
+ISR ( USART_RXC_vect ){
+	
+}
+
+ISR(ADC_vect){
+	test=0;
+	char str[10];
+	uint16_t data = ADCL;
+	data+= (ADCH<<8);
+	PORTB = data>>2;
+	itoa((data*15), str, 10);
+	uart_transmitMult(str);
+	uart_transmit(0x0A);
 }
--- usart/main.c
+++ usart/main.c
@@ -1,15 +1,15 @@
-#define F_CPU 12000000UL
+#define F_CPU 1000000UL
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define BAUD 9600                                   // define baud
+#define BAUD 2400                                   // define baud
 #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)            // set baud rate value for UBRR
-  
+
   void uart_init (void){
     UBRRH = (BAUDRATE>>8);
     UBRRL = BAUDRATE;	// set baud rate
-    UCSRB|= (1<<TXEN)|(1<<RXEN);	// enable receiver and transmitter
+    UCSRB|= (1<<TXEN)|(1<<RXEN)|(1<<RXCIE);	// enable receiver and transmitter
     UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);	// 8bit data format
 }
 
@@ -22,7 +22,7 @@ unsigned char uart_recieve (void){
     while(!(UCSRA) & (1<<RXC));	// wait while data is being received
     return UDR;	// return 8-bit data
 }
-
+volatile uint8_t data;
 volatile uint8_t cnt=0;
 int main(void){
   DDRB |= 1 << PINB0 ;
@@ -33,18 +33,19 @@ int main(void){
   TCCR0 |=(1<<CS02) |(1<<CS00);
   
   while(1){
-	uint8_t data = uart_recieve ();
-	if(data){
+	if(data>0){
 		uart_transmit(data);
 		data=0;
 		}
   }
   return 0;
 }
+ISR(USART_RXC_vect, ISR_BLOCK){
+	data=uart_recieve();
+}
 ISR(TIMER0_OVF_vect){
-	if(cnt>=46){
+	if(cnt>=5){
 		uart_transmit (0x41);
-		uart_transmit ('\n');
 		uart_transmit (0x42);
 		uart_transmit (0x0A);
 		PORTB ^= 1<<PINB0 ;
