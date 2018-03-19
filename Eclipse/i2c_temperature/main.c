0492e581dc54d1704ed326f8a43ad7e765475da6
reivax-boucoi <reivax.boucoi@laposte.net>
2018-03-19 08:39:59 +0100

    updated project

---
+++ Eclipse/i2c_temperature/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "periphs.h"
+#define SLA 0x40
+#define TEMPREG 0xE3
+
+
+float calc(uint16_t v){
+	return ((175.72*v)/65536)-46.85;
+}
+int main(void){
+	DDRB |= (1 << PB0) | (1<<PB1);
+	PORTB |= (1<<PB0) | (0<PB1);
+	DDRC |=(1<<PA7);
+	i2c_init();
+	uart_init();
+	adc_init();
+	sei();
+	while(1){
+		uint16_t data=0;
+		i2c_start((SLA<<1)+I2C_WRITE);
+		i2c_write(TEMPREG);
+		i2c_start((SLA<<1)+I2C_READ);
+		data=(i2c_read_ack()<<8);
+		data|=i2c_read_nack();
+		i2c_stop();
+		float temp = calc(data);
+		if(temp>22.0)PORTC^=(1<<PA7);
+		char str[10] = {0};
+		sprintf(str,"%i\r\n",456);
+		//uart_transmitBuff(str,strlen(str)); // doesn't work
+		uart_transmitNow(str); // works
+		PORTB^=(1<<PB0);
+		_delay_ms(128);
+	}
+	return 0;
+}
+ISR (ADC_vect ){
+	adc_read();
+}
+ISR (TWI_vect ){
+}
+ISR (USART_RXC_vect ){
+	uart_isr_rx();
+}
+ISR (USART_UDRE_vect ){
+	uart_isr_udre();
+}
