2117f23ddead918238c0bd5fb4598f4ebb2b0ac1
reivax-boucoi <reivax.boucoi@laposte.net>
2018-04-07 15:19:27 +0200

    ljhglhjgljhgljhhglhgljhgjuliencombatte

--- Eclipse/Atmega169/main.cpp
+++ Eclipse/Atmega169/main.cpp
@@ -3,43 +3,46 @@
 #include "LCD.h"
 LCD myLCD;
 
-uint16_t integerRoot(uint32_t* nb){
-	int32_t i=15;
-	uint32_t res=(1<<i);
-	while(i>=0){
-		if((res*res)>=(*nb))res-=(1<<i);
-		res+=(1<<--i);
-	}
-	return res;
+uint16_t integerRoot(uint32_t x){
+    uint16_t res=0;
+    uint16_t add= 0x8000;
+    for(int i=0;i<16;i++){
+        uint16_t temp=res | add;
+        uint32_t g2=(temp-1)*(temp-1);
+        if (x>g2){
+            res=temp;
+        }
+        add>>=1;
+    }
+    return res;
 }
 
-bool isPrime(uint32_t *i){
+bool isPrime(uint32_t i){
 	uint16_t max=integerRoot(i);
 	for(uint32_t j=3;j<max;j+=2){
-		if((*i)%j==0)return false;
+		if(i%j==0)return false;
 	}
 	return true;
 }
-bool isPrimeDeprecated(uint32_t *i){
-	uint16_t max=(*i)/2;
-	for(uint32_t j=3;j<max;j+=2){
-		if((*i)%j==0)return false;
-	}
-	return true;
-}
+
 int main(void){
-	//uint32_t i=999979;
-	  uint32_t i=999003;
-	while(1){/*
-		if(prime(i)){
-			myLCD.setNb(i);
-		}
-		i+=2;
-		myLCD.blink(1);
-		if(i>999983)i=3;*/
-		i+=2;
-		if(isPrime(&i))myLCD.setNb(i);
-		if(i>999983)i=3;
+	uint32_t i=3;
+	myLCD.setDigit(3,LCD::Equal);
+		while(1){/*
+			if(prime(i)){
+				myLCD.setNb(i);
+			}
+			i+=2;
+			myLCD.blink(1);
+			if(i>999983)i=3;*/
+			_delay_ms(750);
+			myLCD.setDigit(0,i/100);
+			myLCD.setDigit(1,(i/10)%10);
+			myLCD.setDigit(2,i%10);
+			myLCD.setDigit(4,integerRoot(i)/10);
+			myLCD.setDigit(5,integerRoot(i)%10);
+			i+=1;
+			myLCD.blink(1);
 	}
 	return 0;
 }
