5597be7adec5ee5797d38201f8ad1ddb0d28e91c
reivax-boucoi <reivax.boucoi@laposte.net>
2018-04-16 09:47:54 +0200

    adaaa

--- Eclipse/t85_inductor_sat/main.cpp
+++ Eclipse/t85_inductor_sat/main.cpp
@@ -3,10 +3,10 @@
 
 
 int main(void){
-	DDRB&=~(1<<PB4);
+	DDRB&=~(1<<PB4);//adc input pot
 	DDRB&=~(1<<PB0);
-	PORTB|=(1<<PB0);
-	DDRB|=(1<<PB3);
+	PORTB|=(1<<PB0);//output pulse
+	DDRB|=(1<<PB3);//button toggle
 	ADMUX|=(1<<ADLAR)|(1<<MUX1);//left adjusted input single ended on PB4
 	bool state=false;
 
