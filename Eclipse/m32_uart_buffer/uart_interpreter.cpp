975004be3f536e3a1cbbb065b545e237d2779f4f
reivax-boucoi <reivax.boucoi@laposte.net>
2018-05-15 12:10:00 +0200

    fsdf

---
+++ Eclipse/m32_uart_buffer/uart_interpreter.cpp
@@ -0,0 +1,207 @@
+#include "uart_interpreter.h"
+
+unsigned char* params[MAXPARAM];
+uint8_t nbParams;
+
+void uart_init(void){
+	UBRRH = (BAUDRATE>>8);
+	UBRRL = BAUDRATE;
+	UCSRB = ((1<<RXEN) | (1<<TXEN) | (1<<RXCIE));
+	uint8_t  x = 0;
+	uart_rx_tail = x;
+	uart_rx_head = x;
+	uart_tx_tail = x;
+	uart_tx_head = x;
+}
+
+uint8_t  uart_receiveByte(void){
+	while (uart_rx_head == uart_rx_tail);
+	uart_rx_tail = (uart_rx_tail + 1) & UART_BUFFER_MASK;
+	return uart_buff_rx[uart_rx_tail];
+}
+
+void uart_transmitByte(uint8_t  data){
+	uart_tx_head = (uart_tx_head + 1) & UART_BUFFER_MASK;
+	while (uart_tx_head == uart_tx_tail);
+	uart_buff_tx[uart_tx_head] = data;
+	UARTTXEN();
+}
+
+void uart_transmitNb(uint8_t data,uint8_t mode) {
+	char str[]="0b00000000";
+	switch(mode){
+	case 'H':
+		sprintf(str,"0x%X",data);
+		break;
+	case 'B':
+		str[0]='0';
+		str[1]='b';
+		for(uint8_t i=2;i<10;i++){
+			if(data&(1<<(9-i)))str[i]='1';
+		}
+		break;
+		case 'D':
+		default:
+		sprintf(str,"%u",data);
+		break;
+	}
+	uart_transmit(str);
+}
+
+void uart_transmit(const char* data) {
+	while(*data>0){
+		uart_transmitByte(*data++);
+	}
+}
+
+void uart_transmitln(const char* data) {
+	uart_transmit(data);
+	uart_transmitByte('\r');
+	uart_transmitByte('\n');
+}
+
+void uart_prompt(void) {
+	uart_transmit("\r\n>");
+
+}
+void uart_isr_udre(void) {
+	if (uart_tx_head != uart_tx_tail) {
+		uart_tx_tail = ( uart_tx_tail + 1 ) & UART_BUFFER_MASK;
+		UDR = uart_buff_tx[uart_tx_tail];
+	} else {
+		UARTTXDIS();
+	}
+}
+
+
+void uart_isr_rxc(void) {
+	cli();
+	uart_rx_head = (uart_rx_head + 1) & UART_BUFFER_MASK;
+	if (uart_rx_head == uart_rx_tail) {
+		// Ooops ! buffer overflow !
+		uart_transmitln("Receive buffer full !");
+	}
+	uart_buff_rx[uart_rx_head] = UDR;
+	switch (uart_buff_rx[uart_rx_head]){
+	case '\n':
+	case '\r':
+		cmd_process();
+		break;
+	case KILL:
+		uart_rx_emptyBuffer();
+		uart_transmit("\r\nKilled all running processes");
+		uart_prompt();
+		break;
+	case PREV:
+		uart_rx_loadBuffer(last_cmd);
+		uart_rx_printBuffer();//TODO redraw command
+		break;
+	case BACKSPACE:
+		uart_buff_rx_removeLast();// TODO fix this
+		uart_transmit("\r>");
+		uart_rx_printBuffer();
+		break;
+	default:
+		uart_transmitByte(uart_buff_rx[uart_rx_head]);
+		break;
+	}
+	sei();
+}
+
+uint8_t uart_receivedAvailable(void) {
+	if(uart_rx_head>=uart_rx_tail)	return uart_rx_head-uart_rx_tail;
+	return uart_rx_head + UART_BUFFER_SIZE - uart_rx_tail;
+}
+
+uint8_t uart_transmitAvailable(void) {
+	if(uart_tx_head>=uart_rx_tail)	return uart_tx_head-uart_tx_tail;
+	return uart_tx_head + UART_BUFFER_SIZE - uart_tx_tail;
+}
+
+void uart_rx_printEmptyBuffer(void) {
+	while(uart_receivedAvailable()>0)uart_transmitByte(uart_receiveByte());
+}
+void uart_rx_printBuffer(void) {
+	uart_transmit("");
+	uint8_t i=uart_rx_tail;
+	while(i!=uart_rx_head){
+		uart_transmitByte(uart_buff_rx[i]);
+		i = (i+1) & UART_BUFFER_MASK;
+	}
+}
+void uart_rx_emptyBuffer(void) {
+	while(uart_receivedAvailable()>0)uart_receiveByte();
+}
+void uart_rx_loadBuffer(const unsigned char* s) {
+	for(uint8_t i=0, uart_rx_head=uart_rx_tail;s[i]!=NULLCHAR;i++){
+		uart_buff_rx[uart_rx_head]=s[i];
+		uart_rx_head= (uart_rx_head+1) & UART_BUFFER_MASK;
+	}
+}
+
+void uart_buff_rx_removeLast(void){
+	if(uart_rx_head!=uart_rx_tail){
+		if(uart_rx_head==0){
+			uart_rx_head=UART_BUFFER_MASK;
+		}else{
+			uart_rx_head=uart_rx_head-1;
+		}
+	}
+}
+
+uint8_t cmd_cmp(const char* cmd, const char* entry) {
+	uint8_t c=0;
+	while(cmd[c]!=NULLCHAR && entry[c]!=NULLCHAR){
+		if(cmd[c]!=entry[c])return 0;
+		c++;
+	}
+	if(cmd[c]==NULLCHAR)return 1;
+	return 0;
+}
+
+
+void cmd_process(void) {
+	uint8_t i=0;
+	if(uart_receivedAvailable()<=1){
+		uart_rx_emptyBuffer();
+		uart_prompt();
+		return;
+	}
+	memcpy(last_cmd,cmd_buffer,sizeof(cmd_buffer));
+	while(uart_receivedAvailable()>0){
+		cmd_buffer[i++]=uart_receiveByte();
+	}
+	cmd_buffer[i-1]=NULLCHAR;
+	cmd_parse();
+	for (uint8_t cmd = 0; cmd < NB_COMMANDS; cmd++) {
+
+		if (cmd_cmp(cmd_table[cmd].str, (char *)cmd_buffer)) {
+			cmd_table[cmd].fptr_t();
+			return;
+		}
+	}
+	uart_transmit("\r\nUnknown command, type \"help\" for help");
+	uart_prompt();
+}
+
+void cmd_parse(void) {//TODO check
+	int l;
+	nbParams = 0;
+	for (l=0;l<MAXPARAM;l++)params[l] = NULL;
+
+	for (l = 0; cmd_buffer[l] != NULLCHAR; l++) {
+		if(nbParams>=MAXPARAM){
+			uart_transmitln("Max number of parameters exceeded !");
+			break;
+		}
+		if (cmd_buffer[l] == ' ') {
+			cmd_buffer[l] = NULLCHAR;
+			params[nbParams++] = &cmd_buffer[l] + 1;
+		}
+	}
+
+}
+
+uint8_t param_int(uint8_t nb){
+	return atoi((char*)params[nb]);
+}
---
+++ Eclipse/m32_uart_buffer/uart_interpreter.h
@@ -0,0 +1,66 @@
+#ifndef UART_INTERPRET_H
+#define UART_INTERPRET_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <string.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "cmds.h"
+
+#define BAUD 38400
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
+
+#ifndef NULLCHAR
+#define NULLCHAR '\0'
+#endif
+
+#define KILL 'K'
+#define PREV '&'
+#define BACKSPACE 127
+
+#define UART_BUFFER_SIZE 256     // 2 to 256 bytes
+#define UART_BUFFER_MASK (UART_BUFFER_SIZE - 1)
+#define UARTTXEN()	UCSRB |= (1<<UDRIE)
+#define	UARTTXDIS() UCSRB &= ~(1<<UDRIE)
+
+static  uint8_t  uart_buff_rx[UART_BUFFER_SIZE];
+static volatile uint8_t  uart_rx_head;
+static volatile uint8_t  uart_rx_tail;
+static uint8_t  uart_buff_tx[UART_BUFFER_SIZE];
+static volatile uint8_t  uart_tx_head;
+static volatile uint8_t  uart_tx_tail;
+
+// uart buffering and hardware functions
+void uart_init(void);
+uint8_t  uart_receiveByte(void);
+void uart_transmitByte(uint8_t  data);
+void uart_transmit(const char  *data);
+void uart_transmitNb(uint8_t data, uint8_t mode);
+void uart_transmitln(const char  *data);
+uint8_t uart_receivedAvailable(void);
+uint8_t uart_transmitAvailable(void);
+void uart_buff_rx_removeLast(void);
+void uart_rx_emptyBuffer(void);
+void uart_rx_loadBuffer(const unsigned char* s);
+void uart_rx_printBuffer(void);
+void uart_rx_printEmptyBuffer(void);
+void uart_prompt(void);
+void uart_isr_udre(void);
+void uart_isr_rxc(void);
+
+//uart command interpreter handling
+
+#define MAXPARAM 5
+extern unsigned char* params[MAXPARAM];
+extern uint8_t nbParams;
+static unsigned char cmd_buffer[UART_BUFFER_SIZE];
+static unsigned char last_cmd[UART_BUFFER_SIZE];
+
+uint8_t cmd_cmp(const char* s1,const char* s2);
+void cmd_process(void);
+void cmd_parse(void);
+uint8_t param_int(uint8_t nb);
+
+#endif
+
