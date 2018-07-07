f3f1ff731a59ecf69d8d9ece9673c128dd8cc288
reivax-boucoi <reivax.boucoi@laposte.net>
2018-07-08 00:47:22 +0200

    oci1a on m6490

--- Eclipse/m6490/main.c
+++ Eclipse/m6490/main.c
@@ -1,11 +1,15 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 int main(void){
-	DDRB|=(1<<PB6);
+	DDRB|=(1<<PB6)|(1<<PB7)|(1<<PB5);
+	TCCR1A|=(1<<COM1A0)|(1<<COM1A1);
+	TCCR1B|=(1<<WGM12);
+	OCR1A=0x00FF;
+	TIMSK1|=(1<<TOIE1);
+	sei();
+	TCCR1B|=(1<<CS10);
 	while(1){
-		_delay_ms(500);
-		PORTB^=(1<<PB6);
 	}
 	return 1;
 }
