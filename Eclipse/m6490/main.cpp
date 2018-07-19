0b29f3ada0928e5a293aecb1204f4108667711f0
reivax-boucoi <reivax.boucoi@laposte.net>
2018-07-19 14:06:42 +0200

    up

--- Eclipse/m6490/main.cpp
+++ Eclipse/m6490/main.cpp
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 #include <avr/io.h>
 #include <util/delay.h>
 
 //float LCDval=0.0;
 int main(void){
 	DDRF|=(1<<PF0);
-	LCDCRB|=(1<<LCDMUX0)|(1<<LCDMUX1)|(1<<LCDPM3)|(1<<LCDPM2)|(1<<LCDPM1)|(1<<LCDPM0);
-	LCDFRR|=(1<<LCDPS0)|(1<<LCDPS1)|(1<<LCDPS2)|(1<<LCDCD2);	//8MHz/(4096*8*5)=48.8Hz
-	LCDCCR|=(1<<LCDCC3);
-	LCDCRA|=(1<<LCDEN);
-	//LCDCRA|=(1<<LCDBL);
+
 	while(1){
 		PORTF^=(1<<PF0);
 		LCDDR00=0xFF;
@@ -58,32 +53,3 @@ int main(void){
 	return 1;
 }
 
-=======
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include "include/uart_interpreter.h"
-
-float LCDval=0.0;
-int main(void){
-	uart_init();
-	DDRB|=(1<<PB6)|(1<<PB7)|(1<<PB5);
-	TCCR1A|=(1<<COM1A0)|(1<<COM1A1);
-	TCCR1B|=(1<<WGM12);
-	OCR1A=0x00FF;
-	TIMSK1|=(1<<TOIE1);
-	sei();
-	TCCR1B|=(1<<CS10);
-	uart_transmit("\r\n\tHello World !");
-	uart_prompt();
-	while(1){
-	}
-	return 1;
-}
-
-ISR(USART_RX_vect){
-	uart_isr_rxc();
-}
-ISR(USART_UDRE_vect){
-	uart_isr_udre();
-}
->>>>>>> 9d3b0f646835cac570ebc3fe71356051f60b58ad
