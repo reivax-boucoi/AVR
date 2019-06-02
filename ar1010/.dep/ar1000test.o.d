87db89dfc69c0502f667b41532fa406d4f8e4fa1
reivax-boucoi <reivax.boucoi@laposte.net>
2019-06-03 00:36:15 +0200

    a

---
+++ ar1010/.dep/ar1000test.o.d
@@ -0,0 +1,45 @@
+ar1000test.o: ar1000test.c /usr/lib/avr/include/stdio.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /usr/lib/avr/include/stdint.h /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /usr/lib/gcc/avr/5.4.0/include/stddef.h /usr/lib/avr/include/avr/io.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom1284p.h \
+ /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
+ /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
+ /usr/lib/avr/include/avr/lock.h i2c.h \
+ /usr/lib/avr/include/avr/interrupt.h types.h defs.h
+
+/usr/lib/avr/include/stdio.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/usr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/usr/lib/avr/include/avr/iom1284p.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+i2c.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+types.h:
+
+defs.h:
