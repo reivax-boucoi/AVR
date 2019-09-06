0aca5dd5ad45c35ecda8e4f80553b5752d28f1fb
reivax-boucoi <reivax.boucoi@laposte.net>
2019-09-06 16:25:09 +0200

    a

--- Eclipse/inductor_sat_tester/main.cpp
+++ Eclipse/inductor_sat_tester/main.cpp
@@ -38,15 +38,19 @@ int main(void){
 	DDRB&=~BUTTON;//input button
 	PORTB|=BUTTON;//input button
 	DDRB|=OUT|LED;//output pulse & LED
-	ADMUX|=(1<<REFS0) |(1<<ADLAR)|(1<<MUX1);//left adjusted input single ended on PB4
-	ADCSRA|=(1<<ADEN)|(1<<ADATE)|(1<<ADPS2)|(1<<ADPS0)|(1<<ADPS0)|(1<<ADIE);
-	DIDR0|=(1<<ADC2D);
-	OCR0A=4;
-	TCCR0A|=(1<<COM0A0)|(1<<COM0A1);
-	TIMSK |=(1<< OCIE0A)|(1<<TOIE0);
+	//ADMUX|=(1<<REFS0) |(1<<ADLAR)|(1<<MUX1);//left adjusted input single ended on PB4
+	//ADCSRA|=(1<<ADEN)|(1<<ADATE)|(1<<ADPS2)|(1<<ADPS0)|(1<<ADPS0)|(1<<ADIE);
+	//DIDR0|=(1<<ADC2D);
+
+	OCR0B=254;
+	OCR0A=120;
+	//TCCR0A|=(1<<WGM01);//(1<<COM0B0)|(1<<COM0B1);
+	TIMSK |=(1<<TOIE0);
 	sei();
-	ADCSRA|=(1<<ADSC);
-	TCCR0B |=(1<<CS00);
+
+	//ADCSRA|=(1<<ADSC);
+
+	TCCR0B |=(1<<CS00)|(1<<CS02);
 
 	while(1){
 		toggled();
@@ -54,14 +58,8 @@ int main(void){
 	return 0;
 }
 ISR(TIMER0_OVF_vect){
-	//if(state){
-	//PORTB|=OUT;
-	//}
-}
-ISR(TIMER0_COMPA_vect){
-	//PORTB&=~OUT;
-	if(OCR0A<127)PORTB^=OUT;
+	PORTB
 }
 ISR(ADC_vect){
-	OCR0A=ADCH;
+	//OCR0A=ADCH;
 }
