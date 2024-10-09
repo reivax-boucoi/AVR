b1cca310caef2787ddf07d6d3383f089083b76fd
reivax-boucoi <reivax.boucoi@laposte.net>
2024-10-09 21:23:07 +0200

    a

---
+++ libraries/DigitalIO/src/boards/Attiny1634GpioPinMap.h
@@ -0,0 +1,23 @@
+#ifndef Attiny1634GpioPinMap_h
+#define Attiny1634GpioPinMap_h
+static const GpioPinMap_t GpioPinMap[] = {
+  GPIO_PIN(B, 0),  // D0
+  GPIO_PIN(A, 7),  // D1
+  GPIO_PIN(A, 6),  // D2
+  GPIO_PIN(A, 5),  // D3
+  GPIO_PIN(A, 4),  // D4
+  GPIO_PIN(A, 3),  // D5
+  GPIO_PIN(A, 2),  // D6
+  GPIO_PIN(A, 1),  // D7
+  GPIO_PIN(A, 0),  // D8
+  GPIO_PIN(C, 5),  // D9
+  GPIO_PIN(C, 4),  // D10
+  GPIO_PIN(C, 2),  // D11
+  GPIO_PIN(C, 1),  // D12
+  GPIO_PIN(C, 0),  // D13
+  GPIO_PIN(B, 3),  // D14
+  GPIO_PIN(B, 2),  // D15
+  GPIO_PIN(B, 1),  // D16
+  GPIO_PIN(C, 3)   // D17
+};
+#endif  // Attiny1634GpioPinMap_h
