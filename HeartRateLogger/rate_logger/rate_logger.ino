02eba5eb08a417deade96bcb5acd8de96562c88b
Xavier Bourlot <xavier.bourlot@nxp.com>
2025-11-05 10:40:16 +0100

    added heartrate

--- HeartRateLogger/rate_logger/rate_logger.ino
+++ HeartRateLogger/rate_logger/rate_logger.ino
@@ -1,13 +1,12 @@
 #define F_SAMPLE_US 5
 unsigned long t=0;
+
 void setup(){
 	Serial.begin(115200);
 	 while (!Serial);
-    
 }
 
 void loop(){
-
-		Serial.println(analogRead(A0));
-     delay(5);
+	Serial.println(analogRead(A0));
+    delay(5);
 }
